2617a20cbf68b2d367a4670ecd01038854562e1d
Michael <fend3r2k9@gmail.com>
2019-11-01 21:27:54 -0700

    initial redo

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-from flask import Flask, Response, jsonify, request, render_template, send_file
+from flask import Flask, Response, jsonify, request, render_template, send_file, send_from_directory
 from flask_bootstrap import Bootstrap
 from deploy import deploy
 from os import environ
 import markdown
 import logging
 import json
+import yaml
+import os
 
 log = logging.getLogger(__name__)
 app = Flask(__name__)
 Bootstrap(app)
-
+# posts = yaml.safe_load(open("posts.yml","r"))
 posts = json.load(open("posts.json"))
 # sort new
 posts.reverse()
@@ -24,9 +26,57 @@ def error_handler():
 def static_from_root():
     return send_file(request.path[1:])
 
+@app.route("/static/<path:path>", methods=["GET"])
+def send_static():
+    #return render_template('index.html')
+    return send_from_directory('static', path)
+
+@app.route("/demo", methods=["GET"])
+def demo():
+    #return render_template('index.html')
+    return send_from_directory('vizew', 'single-post.html')
+
+#@app.route("/<any({}):post>".format(str( )[1:-1]))
+#def posts(post):
+#    return "Hello {}!".format(segment)
+
+@app.route("/test", methods=["GET"])
+def test():
+    #return render_template('index.html')
+    with open("static/posts/example.html", "r") as f:
+        post_html = f.read()
+    return render_template('layout.html', post_html=post_html)
+
 @app.route("/", methods=["GET"])
 def home():
     return render_template('index.html', posts=posts)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+@app.route("/profile", methods=["GET"])
+def posts():
+    with open(f"pages/profile.html", "r") as f:
+        post_html = f.read()
+    return render_template('layout.html', post_html=post_html)
+
+@app.route("/posts", methods=["GET"])
+def posts():
+    post_list = os.listdir('posts')
+    return render_template('posts.html', post_list=post_list)
+
+@app.route('/posts/<path:path>')
+def send_post(path):
+    with open(f"static/posts/{path}", "r") as f:
+        post_html = f.read()
+    return render_template('layout.html', post_html=post_html)
+
+@app.route('/<path:path>')
+def catch_all(path):
+    return send_from_directory('vizew', path)
+
+  
 
 @app.route("/blog/<int:pid>", methods=["GET"])
 def get_post(pid):
@@ -38,5 +88,5 @@ def get_post(pid):
     return error_handler()
 
 if __name__ == '__main__':
-    deploy(posts)
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    #deploy(posts)
+    app.run(host='0.0.0.0', port=8080, debug=True)
