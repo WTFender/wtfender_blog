309267c70610672f377596d33b90ac7343a7f347
Michael <fend3r2k9@gmail.com>
2019-11-02 23:30:02 -0700

    major revisions

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python3
 from flask import Flask, request, render_template, send_from_directory
+import xml.etree.cElementTree as ET
+from datetime import datetime
 import logging
 import yaml
 
 log = logging.getLogger(__name__)
 app = Flask(__name__)
 
-paths = yaml.safe_load(open("paths.yml","r"))
+paths = yaml.safe_load(open("paths.yaml","r"))
 posts = paths['posts']
 pages = paths['pages']
+
+def create_sitemap(posts):
+    dt = datetime.now().strftime ("%Y-%m-%d")
+    root = ET.Element('urlset')
+    root.attrib['xmlns:xsi']="http://www.w3.org/2001/XMLSchema-instance"
+    root.attrib['xsi:schemaLocation']="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    root.attrib['xmlns']="http://www.sitemaps.org/schemas/sitemap/0.9"
+    
+    doc = ET.SubElement(root, "url")
+    ET.SubElement(doc, "loc").text = "https://www.wtfender.com/"
+    ET.SubElement(doc, "lastmod").text = dt
+    ET.SubElement(doc, "changefreq").text = "weekly"
+    ET.SubElement(doc, "priority").text = "1.0"
+    
+    for p in posts:
+        doc = ET.SubElement(root, "url")
+        ET.SubElement(doc, "loc").text = f"https://www.wtfender.com/{p}"
+        ET.SubElement(doc, "lastmod").text = dt
+        ET.SubElement(doc, "changefreq").text = "weekly"
+        ET.SubElement(doc, "priority").text = "0.8"
+    tree = ET.ElementTree(root)
+    tree.write('static/sitemap.xml', encoding='utf-8', xml_declaration=True)
 
 def error_handler():
     return app.config.get("DEFAULT_ERROR_MESSAGE") 
@@ -37,4 +61,6 @@ def send_post(path):
 #     return render_template('post.html', post=pages[path])
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    posts.update(pages)
+    create_sitemap(posts)
+    app.run(host='0.0.0.0', port=8080)
